82c5c636401b5b8ee91acc3747f6ecc0969a170e
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-01-03 16:12:14 +0100

    Created bin/ee.py

---
+++ bin/ee.py
@@ -0,0 +1,19 @@
+"""ee
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-1-3"
+
+class ee:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(ee.ee())
+	
+	def ee():
+		"""ee first funtion
+		"""
+		return "ee" #for the test (see test_ee.py)
+		
+if __name__ == "__main__":
+	ee()
